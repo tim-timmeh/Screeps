0a0d4435e4d8464a6e625cb56f109e3fc341f24f
tim-timmeh <39078118+tim-timmeh@users.noreply.github.com>
2022-01-09 11:46:00 +0800

    fixed upgrader bug if over storage reserve trys to spawn creep bigger than available, live

--- miss.Upgrader.js
+++ miss.Upgrader.js
@@ -21,7 +21,7 @@ MissionUpgrader.prototype.initMiss = function () { // Initialize / build objects
   this.storageCapacity = this.storage.store.getCapacity()
   this.upperReserve = this.storageCapacity - (this.storageCapacity / 2); // 2=50% full, 3=66% full etc
   const lowerReserve = 0; //Forces a lower reserve limit to start scaling from. Eg 500k will only start spawning larger creeps from that limit.
-  this.storagePercent = parseFloat(Math.max(0, (this.storage.store.getUsedCapacity() - lowerReserve) / this.upperReserve).toFixed(3)); // % of used storage
+  this.storagePercent = parseFloat(Math.min(1,Math.max(0, (this.storage.store.getUsedCapacity() - lowerReserve) / this.upperReserve)).toFixed(3)); // % of used storage
   this.paveRoad(this.storage, this.controller, 3);
   this.upgraderCap = this.room.controller.level == 8 ? 5 : undefined; // Max 15w per tick on RCL8
 };
