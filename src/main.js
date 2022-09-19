89b33a5847c504fa545d2f172449032348956966
tim-timmeh <39078118+tim-timmeh@users.noreply.github.com>
2022-09-19 17:16:30 +0800

    remove random old screeps file

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-const { printTest, printHello } = require("./functions")
+'use strict';
+// Requires
+const { printTest, printHello } = require("./functions");
+
+// Init heap data
 
 module.exports.loop = function(){
   printTest("arg")
