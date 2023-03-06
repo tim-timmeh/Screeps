6537f9f47007669585d8d9d53200a3073a7d8c80
tim-timmeh <39078118+tim-timmeh@users.noreply.github.com>
2023-03-06 22:07:19 +0800

    junk

--- Reading Material/ScreepsTEST old Refacter/towerAI.js
+++
@@ -1,9 +0,0 @@
-var missTower = require("miss.tower");
-
-function towerAI() {
-  var towers = _.filter(Game.structures, s => s.structureType == STRUCTURE_TOWER);
-  for (var tower of towers) {
-    missTower.run(tower);
-  }
-}
-module.exports = towerAI
--- Reading Material/ScreepsTest Old/myGlobal.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-global.debug = true;
-
-module.exports = {};
