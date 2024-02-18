8f9ff16bf25d8ee043ec9530dd550ec9df70465c
tim-timmeh <39078118+tim-timmeh@users.noreply.github.com>
2024-02-18 18:09:16 +0800

    traveler

--- src/util.require.js
+++ src/util.require.js
@@ -3,4 +3,5 @@ require('./util.globals'); // Global Variables
 require('./util.prototypes'); // Modified Prototypes
 //require('./king') // king constructor
 require('./util.moveToModule'); // creep move prototype
+//require('./traveler'); // creep traveler prototype
 global.excuseMe = require('./util.excuseMe'); // Nudge Creeps out of way.
